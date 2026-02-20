31f72fe63f50a79c910ce0fc884b74c16a710d46
Serhii <21serhii10@gmail.com>
2026-02-20 22:10:01 +0200

    feat: add database connection and Client table

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# My First Project
+import sqlite3
+
+conn = sqlite3.connect('salon.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS "Client" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL
+);
+''')
+conn.commit()
